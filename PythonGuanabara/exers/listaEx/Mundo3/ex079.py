53d38cc19d0231fd18096fd9bf2c2afd7c481d07
Cairo Araujo <cairo.s.santos@gmail.com>
2018-06-21 13:36:12 -0300

    correcao ex 78 e 79 guanabara

--- PythonGuanabara/exers/listaEx/Mundo3/ex079.py
+++ PythonGuanabara/exers/listaEx/Mundo3/ex079.py
@@ -7,9 +7,14 @@ while True:
     else:
         print('Valor ja inserido, não vou adicionar!')
 
+    #if aux not in valores: #Resolução guanabara
+    #    valores.append(aux)
+    #else:
+    #    print('Valor ja inserido, não vou adicionar!')
+
     cont = str(input('Deseja continuar? (S/N) '))
     if cont.lower() == 'n':
         break
 
 valores.sort()
-print(valores)
+print(f'Você adicionou os valores: {valores}')
